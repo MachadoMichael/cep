744f617022f6266fd5acc78e21eb1c0cf241ef9f
Michael <machado.m3@icloud.com>
2024-07-14 10:00:22 -0300

    first version of cep getter package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MachadoMichael/cep
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func main() {
+	for _, url := range os.Args[1:] {
+		req, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "erro ao fazer a requisição: %v \n", err)
+		}
+		defer req.Body.Close()
+
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "erro ao ler a resposta: %v \n", err)
+		}
+
+		var data ViaCEP
+		err = json.Unmarshal(res, &data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "erro ao fazer o parse da requisição: %v \n", err)
+		}
+		fmt.Println(data)
+	}
+}
